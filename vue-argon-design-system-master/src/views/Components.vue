2adaeb622080bc23d7a657b692f5c3d89f011922
hwangjeonghan <hjeonghan0@gmail.com>
2024-07-26 17:39:31 +0900

    first commit

---
+++ vue-argon-design-system-master/src/views/Components.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <hero></hero>
+    <basic-elements></basic-elements>
+    <javascript-components></javascript-components>
+    <carousel></carousel>
+  </div>
+</template>
+<script>
+import Hero from "./components/Hero";
+import BasicElements from "./components/BasicElements";
+
+import JavascriptComponents from "./components/JavascriptComponents";
+import Carousel from "./components/Carousel";
+
+export default {
+  name: "components",
+  components: {
+    Hero,
+    BasicElements,
+    JavascriptComponents,
+    Carousel,
+  },
+};
+</script>
---
+++ vue-argon-design-system-master/src/views/components/Carousel.vue
@@ -0,0 +1,58 @@
+
+
+<template>
+
+
+<div style=" background-image: linear-gradient(-225deg, #FFFEFF 0%, #D7FFFE 100%); ">
+
+    
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="../../../public/img/brand/mang.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../../../public/img/brand/mang.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../../../public/img/brand/mang.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../../../public/img/brand/mang.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
